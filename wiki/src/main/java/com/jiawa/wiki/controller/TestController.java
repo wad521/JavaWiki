146949ef40a1e81cce09d264ec64ced32b16ec56
YangXinglong <1363513229@qq.com>
2021-09-11 15:19:22 +0800

    2-8 增加自定义配置项
    @Value（${"  :默认值"}）

--- wiki/src/main/java/com/jiawa/wiki/controller/TestController.java
+++ wiki/src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,11 +17,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @Value("${test.hello:KKKLL}")
+    private  String testHello;
+
+
     //http://127.0.0.1:8880/hello
 
     @GetMapping("/hello")
     public String hello(){
-        return  "hello  yxl yyds   !!!";
+        return  "hello  yxl yyds   !!!"+testHello;
     }
 
     @PostMapping("/hello/post")
