697a4aafa22a8a1672715add94c90cf274d549e5
YangXinglong <1363513229@qq.com>
2021-09-19 17:36:43 +0800

    4-7 制作the-footer组件

--- wiki/web/src/views/Home.vue
+++ wiki/web/src/views/Home.vue
@@ -9,7 +9,7 @@
       >
         <a-sub-menu key="sub1">
           <template #title>
-            <span><user-outlined/>subnav 112312</span>
+            <span><user-outlined/>subnav 1</span>
           </template>
           <a-menu-item key="1">option1</a-menu-item>
           <a-menu-item key="2">option2</a-menu-item>
