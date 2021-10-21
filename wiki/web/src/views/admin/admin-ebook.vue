5620f2dc7160c27d9f8861ac3dea2cde141e54eb
YangXinglong <1363513229@qq.com>
2021-10-21 11:28:48 +0800

    6-6 电子书管理页面前后端分页整合完成

--- wiki/web/src/views/admin/admin-ebook.vue
+++ wiki/web/src/views/admin/admin-ebook.vue
@@ -43,7 +43,7 @@ export default defineComponent({
     const ebooks = ref();
     const pagination = ref({
       current: 1,
-      pageSize: 3,
+      pageSize: 4,
       total: 0
     });
     const loading = ref(false);
@@ -92,13 +92,20 @@ export default defineComponent({
      **/
     const handleQuery = (params: any) => {
       loading.value = true;
-      axios.get("/ebook/list", params).then((response) => {
+      axios.get("/ebook/list", {
+        params: {
+          //params属性定义在125行
+          page:params.page,
+          size:params.size,
+        }
+      }).then((response) => {
         loading.value = false;
         const data = response.data;
-        ebooks.value = data.content;
+        ebooks.value = data.content.list;
 
         // 重置分页按钮
         pagination.value.current = params.page;
+        pagination.value.total = data.content.total;
       });
     };
 
@@ -114,7 +121,13 @@ export default defineComponent({
     };
 
     onMounted(() => {
-      handleQuery({});
+      handleQuery({
+        //要和请求类的属性名一致
+        //且params的属性定义在这里
+        page: 1,
+        size: pagination.value.pageSize,
+        yxl:10
+      });
     });
 
     return {
