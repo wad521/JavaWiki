fb9df7e0ca6613202d25c57d45536694a49b665e
YangXinglong <1363513229@qq.com>
2021-10-21 08:58:28 +0800

    6-2 增加电子书管理页面。 新页面开发：页面、路由、菜单

--- wiki/web/src/router/index.ts
+++ wiki/web/src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../views/home.vue'
 import About from '../views/about.vue'
+import AdminEbook from '../views/admin/admin-ebook.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -18,6 +19,11 @@ const routes: Array<RouteRecordRaw> = [
 
     //懒加载模式
     //component: () => import(/* webpackChunkName: "about" */ '../views/about.vue')
+  },
+  {
+    path: '/admin/ebook',
+    name: 'AdminEbook',
+    component: AdminEbook
   }
 ]
 
---
+++ wiki/web/src/views/admin/admin-ebook.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>admin-ebook书籍管理页面</h1>
+  </div>
+</template>
