a21052d566a3acae5bbea04123218ff587ba6208
YangXinglong <1363513229@qq.com>
2021-10-21 10:15:33 +0800

    6-4 集成PageHelper插件 将mapper日志级别改为trace，打印所有的sql日志

--- wiki/src/main/java/com/jiawa/wiki/service/EbookService.java
+++ wiki/src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.jiawa.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.jiawa.wiki.domain.Ebook;
 import com.jiawa.wiki.domain.EbookExample;
 import com.jiawa.wiki.mapper.EbookMapper;
 import com.jiawa.wiki.req.EbookReq;
 import com.jiawa.wiki.resp.EbookResp;
 import com.jiawa.wiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -19,10 +23,16 @@ import java.util.List;
 @Service
 public class EbookService {
 
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
     public List<EbookResp> list(EbookReq req){
+
+
+
+
         //mybatis逆向的类
         EbookExample ebookExample = new EbookExample();
         //  .Criteria 可以看做一个where条件
@@ -30,7 +40,15 @@ public class EbookService {
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1,3);
         List<Ebook> ebookslist = ebookMapper.selectByExample(ebookExample);
+
+        PageInfo<Ebook> pageinfo = new PageInfo<>(ebookslist);
+        //建议返回前端的数据为total，这样前端可以自己计算页数；
+        //防止百万级数据由前端分页，服务器宕机；
+        LOG.info("总行数:{}",pageinfo.getTotal());
+        LOG.info("总列数:{}",pageinfo.getPages());
+
 
         //链表复制
 //        List<EbookResp> respList = new ArrayList<>();
