06a220a459d12eb97acc12480d524050f7b352b9
YangXinglong <1363513229@qq.com>
2021-10-22 11:56:21 +0800

    6-10 增加删除电子书功能 防止雪花算法id丢失精度，统一配置；JacksonConfig

--- wiki/src/main/java/com/jiawa/wiki/service/EbookService.java
+++ wiki/src/main/java/com/jiawa/wiki/service/EbookService.java
@@ -118,4 +118,11 @@ public class EbookService {
             ebookMapper.updateByPrimaryKey(ebook);
         }
     }
+
+    /*
+    * 删除
+    * */
+    public void delete(Long id){
+        ebookMapper.deleteByPrimaryKey(id);
+    }
 }
--- wiki/web/src/views/admin/admin-ebook.vue
+++ wiki/web/src/views/admin/admin-ebook.vue
@@ -29,9 +29,17 @@
             <a-button type="primary" @click="edit(record)">
               编辑
             </a-button>
-            <a-button type="danger">
-              删除
-            </a-button>
+
+            <a-popconfirm
+                title="删除后不可恢复，确认删除?"
+                ok-text="是"
+                cancel-text="否"
+                @confirm="handledelete(record.id)"
+            >
+              <a-button type="danger">
+                删除
+              </a-button>
+            </a-popconfirm>
           </a-space>
         </template>
       </a-table>
@@ -58,7 +66,7 @@
         <a-input v-model:value="ebook.category2Id" />
       </a-form-item>
       <a-form-item label="描述">
-        <a-input v-model:value="ebook.desc" type="textarea" />
+        <a-input v-model:value="ebook.description" type="textarea" />
       </a-form-item>
     </a-form>
   </a-modal>
@@ -194,6 +202,20 @@ export default defineComponent({
       ebook.value={};
     };
 
+    const handledelete = (id : number) => {
+      //注意是delete请求
+      axios.delete("/ebook/delete/" + id).then((response) => {
+        const data = response.data;
+        if(data.success){
+          //重新加载列表
+          handleQuery({
+            page: pagination.value.current,
+            size: pagination.value.pageSize,
+          });
+        }
+      });
+    };
+
 
     onMounted(() => {
       handleQuery({
@@ -215,11 +237,14 @@ export default defineComponent({
 
       edit,
       add,
+      handledelete,
+
 
       ebook,
       modalVisible,
       modalLoading,
-      handleModalOk
+      handleModalOk,
+
     }
   }
 });
