d1610fb96bba2b8f756438b2c9a2fc7de97e810e
YangXinglong <1363513229@qq.com>
2021-10-24 16:15:52 +0800

    7-4 分类表格按树形结构展示 增加数组转树形数据工具方法[递归算法]

--- wiki/web/src/views/admin/admin-category.vue
+++ wiki/web/src/views/admin/admin-category.vue
@@ -29,7 +29,7 @@
       <a-table
           :columns="columns"
           :row-key="record => record.id"
-          :data-source="categorys"
+          :data-source="level1"
           :pagination="false"
           :loading="loading"
           @change="handleTableChange"
@@ -117,6 +117,21 @@ export default defineComponent({
       }
     ];
 
+
+    /**
+     * 一级分类树，children属性就是二级分类
+     * [{
+     *   id: "",
+     *   name: "",
+     *   children: [{
+     *     id: "",
+     *     name: "",
+     *   }]
+     * }]
+     */
+    const level1 = ref(); // 一级分类树，children属性就是二级分类
+
+
     /**
      * 数据查询
      **/
@@ -127,6 +142,12 @@ export default defineComponent({
         const data = response.data;
         if (data.success) {
           categorys.value = data.content;
+
+          console.log("原始数组：", categorys.value);
+          level1.value = [];
+          level1.value = Tool.array2Tree(categorys.value, 0);
+          console.log("树形结构：", level1);
+
         } else {
           message.error(data.message);
         }
@@ -196,7 +217,8 @@ export default defineComponent({
     return {
       //定义的响应式变量，html如果要用一定记得return
       param,
-      categorys,
+      //categorys,
+      level1,
       columns,
       loading,
       handleQuery,
